341f9d4664ded285bb3daac41aacfb3a037a6e91
git <git@github.com>
2020-05-03 16:11:55 -0400

    Annotate

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -1,15 +1,11 @@
 # == Schema Information
 #
-# Table name: creatures
+# Table name: encounters
 #
-#  id               :integer          not null, primary key
-#  name             :string           not null
-#  type             :string           not null
-#  armor_class      :integer          not null
-#  initiative_bonus :integer
-#  advantage        :boolean
-#  initiative_value :integer
-#  user_id          :integer
+#  id            :integer          not null, primary key
+#  name          :string           not null
+#  history       :json
+#  game_space_id :integer
 #
 
 class Encounter < ApplicationRecord
