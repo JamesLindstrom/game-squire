33955742276d8325bbc5d0236c505a934456875f
git <git@github.com>
2020-05-04 13:45:43 -0400

    Fix Devise issue with tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,8 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # Include devise helpers
+  config.include Devise::Test::ControllerHelpers, type: :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
