35c43f781378b47bf145060fd129678918ce3d37
git <git@github.com>
2020-05-05 13:19:27 -0400

    Add render_error_messages helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def render_error_messages(record, field)
+    return if record.errors[field].none?
+
+    error_string = ''
+    record.errors[field].each do |error|
+      error_string += "#{field.to_s.humanize.titleize} #{error}. "
+    end
+    content_tag(:error, error_string)
+  end
 end
