9d1e361d27d16d4b0447bb05046f042ab8c56997
git <git@github.com>
2020-05-05 19:23:03 -0400

    Install Foundation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ require("@rails/activestorage").start()
 require("channels")
 require('jquery')
 
+import "foundation-sites"
+require("src/application")
+Foundation.addToJquery($)
+$(document).foundation();
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
