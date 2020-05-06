0d786c662d7317b5ba6f353aaea0291f1a420660
git <git@github.com>
2020-05-06 06:26:38 -0400

    Add next_turn method to encounter

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -37,4 +37,30 @@ class Encounter < ApplicationRecord
     initiative_array.sort! { |a, b| b[:result] <=> a[:result] }
     self.update(initiative_order: initiative_array)
   end
+
+  def next_turn
+    return false unless initiative_order&.any?
+
+    if self.history&.any?
+      # Get the index of the last creature to have a turn
+      last_creature_id = self.history.last['creature_id']
+      last_creature_index = initiative_order.index do |order_entry|
+        order_entry['creature_id'] == last_creature_id
+      end
+
+      # Use the index to find out who should be next
+      unless initiative_order[last_creature_index + 1].nil?
+        next_creature_id = initiative_order[last_creature_index + 1]['creature_id']
+      else
+        next_creature_id = initiative_order[0]['creature_id']
+      end
+    else
+      self.history = []
+      next_creature_id = initiative_order.first['creature_id']
+    end
+    # I know I'm not storing much data in this JSON right now, but I think I'll
+    # need to add more as I add more features to the tracker.
+    self.history << { creature_id: next_creature_id }
+    self.save
+  end
 end
--- spec/models/encounter_spec.rb
+++ spec/models/encounter_spec.rb
@@ -51,5 +51,45 @@ RSpec.describe Encounter, type: :model do
         expect(encounter.initiative_order[1]['result']).to be >= encounter.initiative_order[2]['result']
       end
     end
+
+    describe 'next_turn' do
+      before(:each) do
+        @user = create(:user)
+        @player = create(:creature, :player)
+        @npc = create(:creature, :npc)
+        @event = create(:creature, :event)
+        @game_space = create(:game_space, user: @user)
+        @encounter = create(:encounter, game_space: @game_space)
+        @encounter.creatures = [@player, @npc, @event]
+        @encounter.roll_initiative
+      end
+
+      context 'when no turns have been taken' do
+        it 'should add the first creature in initiative order to history' do
+          expect(@encounter.history).to be_nil
+          @encounter.next_turn
+          expect(@encounter.history.last['creature_id']).to eq @encounter.initiative_order.first['creature_id']
+        end
+      end
+
+      context 'when some turns have been taken' do
+        it 'should add the next creature in initiative order to history' do
+          @encounter.next_turn
+          expect(@encounter.history).to_not be_nil
+          @encounter.next_turn
+          expect(@encounter.history.last['creature_id']).to eq @encounter.initiative_order[1]['creature_id']
+        end
+      end
+
+      context 'when every creature has gone once' do
+        it 'should start over from the begining of initiative order' do
+          length_of_init_order = @encounter.initiative_order.size
+          length_of_init_order.times { @encounter.next_turn }
+          expect(@encounter.history.size).to eq @encounter.initiative_order.size
+          @encounter.next_turn
+          expect(@encounter.history.last['creature_id']).to eq @encounter.initiative_order.first['creature_id']
+        end
+      end
+    end
   end
 end
